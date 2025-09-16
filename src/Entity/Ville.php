ea78cc05baf9ef8296b07ee5392e9aae0e69a6df
Julia <julia.taylor2023@campus-eni.fr>
2025-09-16 16:46:10 +0200

    modif BDD

--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -27,16 +27,9 @@ class Ville
     #[ORM\OneToMany(targetEntity: Lieu::class, mappedBy: 'ville')]
     private Collection $Lieus;
 
-    /**
-     * @var Collection<int, Utilisateur>
-     */
-    #[ORM\OneToMany(targetEntity: Utilisateur::class, mappedBy: 'Ville')]
-    private Collection $utilisateurs;
-
     public function __construct()
     {
         $this->Lieus = new ArrayCollection();
-        $this->utilisateurs = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -92,36 +85,6 @@ class Ville
             // set the owning side to null (unless already changed)
             if ($lieu->getVille() === $this) {
                 $lieu->setVille(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Utilisateur>
-     */
-    public function getUtilisateurs(): Collection
-    {
-        return $this->utilisateurs;
-    }
-
-    public function addUtilisateur(Utilisateur $utilisateur): static
-    {
-        if (!$this->utilisateurs->contains($utilisateur)) {
-            $this->utilisateurs->add($utilisateur);
-            $utilisateur->setVille($this);
-        }
-
-        return $this;
-    }
-
-    public function removeUtilisateur(Utilisateur $utilisateur): static
-    {
-        if ($this->utilisateurs->removeElement($utilisateur)) {
-            // set the owning side to null (unless already changed)
-            if ($utilisateur->getVille() === $this) {
-                $utilisateur->setVille(null);
             }
         }
 
