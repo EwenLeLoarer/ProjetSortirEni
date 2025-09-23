a196f28b97e36a9c7da38efa36545b7bee5236c2
elefloch2024 <ewen.lefloch2024@campus-eni.fr>
2025-09-23 16:13:41 +0200

    gestion des villes et gestion des sorties

--- src/Controller/VilleController.php
+++ src/Controller/VilleController.php
@@ -4,21 +4,52 @@ namespace App\Controller;
 
 use App\Entity\Ville;
 use App\Form\VilleType;
+use App\Repository\VilleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 final class VilleController extends AbstractController
 {
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/ville', name: 'app_ville')]
+    public function index(VilleRepository $repo, Request $request): Response
+    {
+        $query = $request->query->get('query');
+        $villes = $repo->findVilles($query);
+        $form = $this->createForm(VilleType::class, new Ville(), [
+            'action' => $this->generateUrl('app_create_ville'),
+            'method' => 'POST',
+        ]);
+        $editForms = [];
+        foreach ($villes as $v) {
+            $editForms[$v->getId()] = $this->createForm(VilleType::class, $v, [
+                'action' => $this->generateUrl('app_edit_ville', ['id' => $v->getId()]),
+                'method' => 'POST',
+            ])->createView();
+        }
+
+        if ($request->isXmlHttpRequest()) {
+            $html = $this->renderView('ville/_table.html.twig', [
+                'villes' => $villes,
+                'form' => $form->createView(),
+                'editForms' => $editForms,
+            ]);
+            return new Response($html);
+        }
+        return $this->render('ville/index.html.twig', [
+            'villes' => $villes,
+            'form' => $form->createView(),
+            'editForms' => $editForms,
+        ]);
+    }
     #[Route('/ville/create', name: 'app_create_ville')]
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
         $ville = new Ville();
-
         $form = $this->createForm(VilleType::class, $ville);
-
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
@@ -30,15 +61,50 @@ final class VilleController extends AbstractController
                     'newVilleId' => $ville->getId(),
                 ]);
             }
-
+            if ($request->isXmlHttpRequest()) {
+                return new Response('',204);
+            }
             return $this->redirectToRoute('app_home');
         }
 
-
-
-
-        return $this->render('ville/index.html.twig', [
+        return $this->render('ville/create.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    #[Route('/ville/{id}/edit', name: 'app_edit_ville')]
+    public function edit(Request $request, EntityManagerInterface $em, Ville $ville): Response
+    {
+        $form = $this->createForm(VilleType::class, $ville)->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $em->flush();
+
+            if($request->isXmlHttpRequest()) {
+                return new Response('',204);
+            }
+
+            return $this->redirectToRoute('app_ville');
+
+        }
+        return $this->render('ville/edit.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/ville/{id}/delete', name: 'app_delete_ville')]
+    public function delete(Request $request, Ville $ville, EntityManagerInterface $em): Response
+    {
+        if (!$this->isCsrfTokenValid('delete_ville_'.$ville->getId(), $request->request->get('_token'))) {
+            return $request->isXmlHttpRequest() ? new Response('', 400) : $this->redirectToRoute('app_ville');
+        }
+
+        $em->remove($ville);
+        $em->flush();
+
+        if ($request->isXmlHttpRequest()) {
+            return new Response('',204);
+        }
+
+        return $this->redirectToRoute('app_ville');
     }
 }
