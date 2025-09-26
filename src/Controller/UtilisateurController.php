1ad23ae1e6032c1b775d596c67ee55705273b657
elefloch2024 <ewen.lefloch2024@campus-eni.fr>
2025-09-26 09:16:21 +0200

    style profil et modification

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Utilisateur;
 use App\Form\UtilisateurType;
-use App\Repository\UtilisateurRepository;
 use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,37 +12,44 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 final class UtilisateurController extends AbstractController
 {
-    #[Route('/profil/edit', name: 'app_profil')]
-    public function edit( Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher, FileUploader $fileUploader): Response
+        #[Route('/profil/edit', name: 'app_profil')]
+        public function edit(Request $request, EntityManagerInterface $em,
+                     UserPasswordHasherInterface $passwordHasher, FileUploader $fileUploader): Response
     {
         $utilisateur = $this->getUser();
-        $utilisateurForm = $this->createForm(UtilisateurType::class, $utilisateur);
-        $utilisateurForm->handleRequest($request);
-        if ($utilisateurForm->isSubmitted() && $utilisateurForm->isValid()) {
-            $imageFile = $utilisateurForm->get('photo')->getData();
-            if ($imageFile) {
-                $utilisateur->setPhoto($fileUploader->upload($imageFile));
-            }
-            $plainPassword = $utilisateurForm->get('password')->getData();
-            $passwordConfirmation = $utilisateurForm->get('passwordConfirmation')->getData();
 
-            if ($plainPassword) {
-                if ($plainPassword !== $passwordConfirmation) {
-                    $utilisateurForm->get('passwordConfirmation')->addError(new FormError('Les mots de passe ne correspondent pas.'));
-                } else {
-                    $encodedPassword = $passwordHasher->hashPassword($utilisateur, $plainPassword);
-                    $utilisateur->setPassword($encodedPassword);
-                }
+        $form = $this->createForm(UtilisateurType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        $plain = $form->get('password')->getData();
+        $confirm = $form->get('passwordConfirmation')->getData();
+
+        if ($plain && $plain !== $confirm) {
+            $form->get('passwordConfirmation')->addError(new FormError('Les mots de passe ne correspondent pas.'));
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($file = $form->get('photo')->getData()) {
+                $utilisateur->setPhoto($fileUploader->upload($file));
             }
+            if ($plain) {
+                $utilisateur->setPassword($passwordHasher->hashPassword($utilisateur, $plain));
+            }
+
+            $em->persist($utilisateur);
             $em->flush();
+
             $this->addFlash('success', "La mise à jour du profil a bien été prise en compte.");
             return $this->redirectToRoute('app_profil');
         }
-        return $this->render('utilisateur/edit.html.twig', ['utilisateurForm' => $utilisateurForm, 'utilisateur' => $utilisateur]);
+
+        return $this->render('utilisateur/edit.html.twig', [
+            'utilisateurForm' => $form,
+            'utilisateur' => $utilisateur,
+        ]);
     }
 
     #[Route('/utilisateur/{id}', name: 'app_utilisateur', requirements: ['id' => '\d+'])]
